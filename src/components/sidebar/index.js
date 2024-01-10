761b3725b13a487ecda254e886f8070e231e4fdb
Numan Nayeem <72100315+numaan0@users.noreply.github.com>
2024-01-10 11:26:31 +0530

    Dialog Box Changes

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,26 +7,45 @@ import { FaMusic } from 'react-icons/fa';
 import { FaPlus } from 'react-icons/fa';
 import { FaPlay } from 'react-icons/fa';
 import yt_icon from '../../assets/other/yt_icon.png';
+import {
+    Button,
+    Dialog,
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Input,
+  } from "@material-tailwind/react";
 import './style.css'; 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [open, setDialog] = React.useState(false);
 
     const handleSideBar = () => {
         setIsOpen(!isOpen)
     }
 
+    const handleDialog = () => {
+        setDialog(!open);
+        const content = document.getElementById('page-content');
+        if (!open) {
+            // If the dialog is about to open
+            content.classList.add('dialog-open');
+        } else {
+            // If the dialog is about to close
+            content.classList.remove('dialog-open');
+        }
+    }
     const playLists = [{'title':'Liked Music','name':'Auto Playlist'},{'title':'My Music','name':'Numan Nayeem'}
     ,{'title':'Prophec','name':'Numan Numyeem'},{'title':'Unknown Music','name':'Playlist'},
     ,{'title':'Prophec','name':'Numan Numyeem'},{'title':'Unknown Music','name':'Playlist'},
     ,{'title':'Prophec','name':'Numan Numyeem'},{'title':'Unknown Music','name':'Playlist'},
-    ,{'title':'Prophec','name':'Numan Numyeem'},{'title':'Unknown Music','name':'Playlist'}
+    ,{'title':'Prophec','name':'Numan Numyeem'},{'title':'Unknown Music','name':'Playlist'}]
 
-
-]
 
 
     return (
-        <div className="w-1/6 text-white h-screen p-7 border-r border-gray-600">
+        <div className="w-1/6 text-white h-screen p-7 border-r border-gray-600" id="page-content">
             <div className="border-b border-gray-200 pb-5">
                 <div className="flex flex-row">
                     <div className="p-3 text-2xl hover:bg-gray-500 rounded-2xl " onClick={handleSideBar}>
@@ -53,9 +72,11 @@ const Sidebar = () => {
                 </div>
             </div>
             <div className="p-2 mt-2">
+                <Button onClick={handleDialog} variant="gradient">
                 <div className="flex flex-row bg-gray-600 rounded-lg justify-center items-center p-2 hover:bg-gray-500 cursor-pointer">
                    <span className="mr-2"><FaPlus /></span> <p className="">New playlist</p>
                 </div>
+                </Button> 
             </div>
 
             <div className="p-2 overflow-scroll  scrollbar-thumb-gray-400 scrollbar-thumb max-h-[20rem]">
@@ -71,6 +92,29 @@ const Sidebar = () => {
                     </div>
                 ))}
             </div>
+
+            <Dialog
+        size="xs"
+        open={open}
+        handler={handleDialog}
+        className="bg-transparent shadow-none fixed inset-0 flex items-center justify-center z-8 rounded-xl"
+      >
+        <Card className="mx-auto w-full max-w-[24rem] bg-gray-400 text-white">
+          <CardBody className="flex flex-col gap-4">
+            <Typography variant="h4" color="blue-gray">
+              New Playlist
+            </Typography>
+            <Input size="xl" placeholder="Title" className="w-full placeholder-white outline-none border-b-2"/>
+            <Input  size="xl" placeholder="Description" className="w-full placeholder-white outline-none border-t-2" />
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Button variant="gradient" onClick={handleDialog} fullWidth>
+              Sign In
+            </Button>
+          </CardFooter>
+        </Card>
+      </Dialog>
+
         </div>
     );
 };
